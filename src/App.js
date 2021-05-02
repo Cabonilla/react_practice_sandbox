7cec7fc14e4543a722eb0fba5f71517cdcf7ee6d
cabonilla <christian.bonilla01@gmail.com>
2021-05-01 18:38:10 -0700

    Organized Components and updated function across components.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import React from 'react';
 import './App.css';
-import Expenses from './components/Expenses';
+import Expenses from './components/Expenses/Expenses';
 
-function App() {
+const App = () => {
   const expenses = [
     {title: 'Home Insurance', amount: 100.00, date: new Date(2021, 0, 12)},
     {title: 'Auto Insurance', amount: 200.00, date: new Date(2021, 0, 13)},
     {title: 'Dog Food', amount: 150.00, date: new Date(2021, 3, 1)},
     {title: 'Cat Food', amount: 300.00, date: new Date(2021, 4, 2)},
   ]
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's Get Started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // )
 
   return (
     <div>
